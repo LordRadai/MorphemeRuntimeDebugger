fc9ef1594ad012361e746d058129844a69975c69
Radai <lordradai@null.net>
2023-09-02 13:49:36 +0200

    Delete src/Morpheme4NetworkInspector directory

--- src/Morpheme4NetworkInspector/MorphemeInterface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Morpheme.h"
-
-namespace MorphemeInterface
-{
-	void drawSkeleton(uint64_t character_ctrl);
-	int getNodeContentAmount(Morpheme::NodeDef* node);
-	void renderNodeAttributeType(Morpheme::NodeDef* node, int data_amount);
-}
